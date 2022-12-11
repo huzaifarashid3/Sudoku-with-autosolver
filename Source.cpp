7d2a67536ff59d495c782b60f6b9894476a2c1de
huzaifarashid3 <40166851+huzaifarashid3@users.noreply.github.com>
2022-12-11 07:53:30 +0500

    added different sizes

--- Source.cpp
+++ Source.cpp
@@ -3,27 +3,58 @@
 #include "olcPixelGameEngine.h"
 using namespace std;
 
+
 class Game :public olc::PixelGameEngine
 {
 private:
-	int arr[9][9] = { 0 };
-	int width = 9;
-	int height = 9;
+	int N;
+	int SRN;
+	int** arr;
+	int width;
+	int height;
 	olc::vf2d borders = { 2,2 };
 	olc::vf2d displace = { 20,80 };
-	bool wow = true;
-	olc::vf2d size = { 50,50 };
+	bool firsttime = true;
+	olc::vf2d size;
 public:
 	bool OnUserCreate() override
 	{
+		N = 16;
+		SRN = sqrt(N);
+
+		size = { 40,40 };
+
+		arr = new int* [N];
+		for (int i = 0; i < N; i++)
+			arr[i] = new int[N];
+
+		width = N;
+		height = N;
+
+		for (int y = 0; y < N; y++)
+			for (int x = 0; x < N; x++)
+				arr[y][x] = 0;
+
+
 		return true;
 	}
 	bool OnUserUpdate(float ftime) override
 	{
+		
+		if (firsttime)
+		{
+			StartingScreen();
+			return true;
 
-		olc::vf2d selected = GetMousePos()-displace;
+		}
+
+		olc::vf2d selected = GetMousePos() - displace;
+
 
 		selected = selected / size;
+		
+
+
 
 		if (selected.x > 0 && selected.x < width && selected.y > 0 && selected.y < height)
 		{
@@ -48,50 +79,128 @@ public:
 			else if (GetKey(olc::Key::NP0).bReleased)
 				arr[int(selected.y)][int(selected.x)] = 0;
 		}
-		
- 		if (GetKey(olc::Key::SPACE).bReleased)
+
+		if (GetKey(olc::Key::SPACE).bReleased)
 		{
-			BackTrackSolver(0,0);
+			BackTrackSolver(0, 0);
 		}
 
 		if (GetKey(olc::Key::R).bReleased)
 			Reset();
-	
+
+
+
+
 		Clear(olc::BLACK);
 
-		if (win() && !check())
-			DrawString(120,30, "SUDDDDOKO!", olc::GREEN,3);
-		else if (check())
-			DrawString(120,30, "SUDDDDOKO!", olc::RED,3);
-		else
-			DrawString(120,30, "SUDDDDOKO!", olc::YELLOW,3);
 
+
+
+		if (win() && !check())
+			DrawString(120, 30, "SUDDDDOKO!", olc::GREEN, 3);
+		else if (check())
+			DrawString(120, 30, "SUDDDDOKO!", olc::RED, 3);
+		else
+			DrawString(120, 30, "SUDDDDOKO!", olc::YELLOW, 3);
+
+
+
+
+		//draw squares in the background
 		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
-			{	
+			{
 				FillRect(olc::vi2d(x, y) * size + borders + displace, size - 2 * borders, olc::DARK_BLUE);
 				FillRect(olc::vi2d(selected.x, selected.y) * size + borders + displace, size - 2 * borders, olc::BLUE);
 			}
 		}
 
-		for (int x = 0; x <= width; x += 3)
+
+		//draw white lines  between the smaller grids
+		for (int x = 0; x <= width; x += SRN)
 		{
 			DrawLine(olc::vf2d(x, 0) * size + displace, olc::vf2d(x, height) * size + displace);
 			DrawLine(olc::vf2d(0, x) * size + displace, olc::vf2d(width, x) * size + displace);
 		}
 
 
+		//draw the value in the array on top of the squares
 		for (int y = 0; y < height; y++)
 		{
 			for (int x = 0; x < width; x++)
 			{
-				DrawString(olc::vi2d(x, y) * size + size / 2 + displace, to_string(arr[y][x]), olc::YELLOW);
+				DrawString(olc::vi2d(x, y) * size + size / 2 + displace, to_string(arr[y][x]), olc::YELLOW,2);
 			}
 		}
 
 
 		return true;
+	}
+
+
+	void StartingScreen()
+	{
+
+		bool chosen = false;
+
+		olc::vi2d selected = GetMousePos();
+		selected = selected / size;
+
+		if (selected == olc::vi2d( width / 3,  height / 2) && GetMouse(0).bPressed)
+		{
+			N = 4;
+			SRN = 2;
+			height = N;
+			width = N;
+			size = { 120,120 };
+			chosen = true;
+		}
+
+
+		if (selected == olc::vi2d(width / 2, height / 2) && GetMouse(0).bPressed)
+		{
+			N = 9;
+			SRN = 3;
+			height = N;
+			width = N;
+			size = { 60,60 };
+			chosen = true;
+		}
+
+
+		if (selected == olc::vi2d(2* width / 3, height / 2) && GetMouse(0).bPressed)
+		{
+			N = 16;
+			SRN = 4;
+			height = N;
+			width = N;
+			chosen = true;
+		}
+
+
+
+
+		Clear(olc::DARK_BLUE);
+
+
+		DrawString(olc::vf2d(size.x * width / 4, size.y * height / 3), "Choose Sudoku Size",olc::WHITE,2);
+		
+		FillRect(olc::vf2d(size.x * width / 3, size.y * height / 2), size);
+		DrawString(olc::vf2d(size.x * width / 3, size.y * height / 2) , "4x4", olc::BLUE, 1);
+
+		FillRect(olc::vf2d(size.x * width / 2, size.y * height / 2), size);
+		DrawString(olc::vf2d(size.x * width / 2, size.y * height / 2) , "9x9", olc::BLUE, 1);
+
+		FillRect(olc::vf2d(size.x *2* width / 3, size.y * height / 2), size);
+		DrawString(olc::vf2d(size.x * 2 * width / 3, size.y * height / 2) , "16x16",olc::BLUE, 1);
+
+
+		FillCircle(GetMouseX(),GetMouseY(), 10, olc::YELLOW);
+
+		if (chosen)
+			firsttime = false;
+		
 	}
 
 
@@ -119,13 +228,13 @@ public:
 						return true;
 			}
 		}
-		//grids	
-		for (int b = 0;b<height;b+=3)
-			for (int a = 0;a<width;a+=3)
-				for (int k = b;k<3+b;k++)
-					for (int l = a;l<3+a;l++)
-						for (int i = b; i < 3+b; i++)
-							for (int j = a; j < 3+a; j++)
+		//smaller grids	
+		for (int b = 0; b < height; b += SRN)
+			for (int a = 0; a < width; a += SRN)
+				for (int k = b; k < SRN + b; k++)
+					for (int l = a; l < SRN + a; l++)
+						for (int i = b; i < SRN + b; i++)
+							for (int j = a; j < SRN + a; j++)
 							{
 								if (i == k && j == l)
 									continue;
@@ -136,17 +245,17 @@ public:
 
 		return false;
 
-		
+
 	}
 
 	bool BackTrackSolver(int i, int j)
 	{
-		
 
-		if (i == 9)
+
+		if (i == N)
 			return true;
 
-	
+
 
 		for (int k = 1; k <= 9; k++)
 		{
@@ -155,7 +264,7 @@ public:
 			if (check())
 				continue;
 
-			if (j == 8)
+			if (j == N-1)
 			{
 				if (BackTrackSolver(i + 1, 0))
 					return true;
@@ -176,17 +285,30 @@ public:
 
 	void Reset()
 	{
-		for (int i = 0; i < 9; i++)
-			for (int j = 0; j < 9; j++)
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
 				arr[i][j] = 0;
 	}
 
 	bool win()
 	{
-		for (int i = 0; i < 9; i++)
-			for (int j = 0; j < 9; j++)
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
 				if (arr[i][j] == 0)
 					return false;
+		return true;
+	}
+
+
+	bool OnUserDestroy() override
+	{
+		for (int i = 0; i < N; i++)
+			delete[] arr[i];
+
+		delete[] arr;
+
+		return true;
+
 	}
 
 };
@@ -195,8 +317,8 @@ public:
 int main(void)
 {
 	Game game;
-	if (game.Construct(500, 540, 1, 1))
+	if (game.Construct(720, 760, 1, 1))
 		game.Start();
-	
+
 
 }
